323c4218284438db7e7fad5b493a308dd36e0d66
Robin <lotosv2010@163.com>
2026-06-17 11:00:55 +0800

    fix(header): 优化直辖市显示逻辑，修复30天预报导航参数
    - 直辖市 province === city 时去掉重复的省级显示
    - 修复 daily 组件 navigator URL 参数格式

--- components/page-header/index.js
+++ components/page-header/index.js
@@ -13,5 +13,18 @@ Component({
       type: String,
       value: ''
     }
+  },
+  data: {
+    displayCity: ''
+  },
+  observers: {
+    'district, city, province': function(district, city, province) {
+      // 直辖市：province === city，去掉重复的省级
+      if (province && city && province === city) {
+        this.setData({ displayCity: '' });
+      } else {
+        this.setData({ displayCity: city });
+      }
+    }
   }
 })
